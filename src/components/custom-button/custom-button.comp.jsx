98c3305d49036d2d8a1ed2ef19a881d2751f80c1
Ladislav Hric <MAC_PRO@work-cs6.home>
2021-02-26 15:20:46 +0000

    bttn

--- src/components/custom-button/custom-button.comp.jsx
+++ src/components/custom-button/custom-button.comp.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children,isGoogleSignIn,  ...otherProps }) => (
-  <button
+  <CustomButton type="button"
     className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
     {...otherProps}
   >
     {children}
-  </button>
+  </CustomButton>
 );
 
 export default CustomButton;
